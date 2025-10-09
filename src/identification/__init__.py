e1504d9e48b6a2c91f0719e32e97aa82c2c8eda1
Tran Quoc Hieu <ngoquochieu1211@gmail.com>
2025-10-09 16:40:47 +0700

    feat: add new identifiers: morphology, hypothesis, cluster

--- src/identification/__init__.py
+++ src/identification/__init__.py
@@ -1,5 +1,8 @@
 from .base import BaseStormIdentifier
 from .simple_identify import SimpleContourIdentifier
-from .subcells_identify import SubcellStormIdentifier
+from .hypothesis_identify import HypothesisIdentifier
+from .morphology__identify import MorphContourIdentifier
+from .cluster_identify import ClusterIdentifier
 
-__all__ = ["extract_contour_by_dbz", "BaseStormIdentifier", "SimpleContourIdentifier", "SubcellStormIdentifier"]
+__all__ = ["extract_contour_by_dbz", "BaseStormIdentifier", "SimpleContourIdentifier", 
+           "HypothesisIdentifier", "MorphContourIdentifier", "ClusterIdentifier"]
