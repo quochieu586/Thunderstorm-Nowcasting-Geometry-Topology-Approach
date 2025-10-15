c7af08bb2b9540dd543caf2421f386788dc38cde
Tran Quoc Hieu <ngoquochieu1211@gmail.com>
2025-10-15 14:37:34 +0700

    feat: implement ETITAN

--- src/cores/similarity/area_intersection.py
+++ src/cores/similarity/area_intersection.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
-
 from src.cores.contours import StormObject
+from typing import Union
+from shapely import Polygon
 
 def area_intersection(storm1: StormObject, storm2: StormObject) -> float:
     """
@@ -29,3 +30,31 @@ def area_intersection(storm1: StormObject, storm2: StormObject) -> float:
     area = cv2.countNonZero(intersection)
 
     return area
+
+def area_overlapping_ratio(
+        storm1: Union[StormObject, Polygon], storm2: Union[StormObject, Polygon], mode: str = 'avg'
+    ) -> float:
+    """
+    Compute overlapping ratio between 2 storms.
+
+    Args:
+        storm1 (Union[StormObject, Polygon]): The first storm object or polygon.
+        storm2 (Union[StormObject, Polygon]): The second storm object or polygon.
+        mode (str, optional): The mode to compute the overlapping ratio. 
+                              Options are 'left', 'right', 'min', 'max', 'avg'. Defaults to 'avg'.
+    """
+    polyg_1 = storm1.polygon if isinstance(storm1, StormObject) else storm1
+    polyg_2 = storm2.polygon if isinstance(storm2, StormObject) else storm2
+
+    if mode == 'avg':
+        return (polyg_1.intersection(polyg_2).area / polyg_1.area + polyg_1.intersection(polyg_2).area / polyg_2.area) / 2
+    elif mode == 'left':
+        return polyg_1.intersection(polyg_2).area / polyg_1.area
+    elif mode == 'right':
+        return polyg_1.intersection(polyg_2).area / polyg_2.area
+    elif mode == 'min':
+        return polyg_1.intersection(polyg_2).area / max(polyg_1.area, polyg_2.area)
+    elif mode == 'max':
+        return polyg_1.intersection(polyg_2).area / min(polyg_1.area, polyg_2.area)
+    else:
+        raise ValueError(f"Invalid mode: {mode}. Choose from 'left', 'right', 'min', 'max', 'avg'.")
