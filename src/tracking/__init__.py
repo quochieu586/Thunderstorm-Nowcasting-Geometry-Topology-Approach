5af8ff680a87bb180012b71030590ddbece851ee
Tran Quoc Hieu <ngoquochieu1211@gmail.com>
2025-10-17 15:12:33 +0700

    feat: implement ImprovedSCIT_2010

--- src/tracking/__init__.py
+++ src/tracking/__init__.py
@@ -1,3 +1,4 @@
 from .base import BaseMatcher, BaseTrackingHistory, BaseTracker
+from .hungarian_matching import reduced_soft_hungarian
 
-__all__ = ["BaseMatcher", "BaseTrackingHistory", "BaseTracker"]
+__all__ = ["BaseMatcher", "BaseTrackingHistory", "BaseTracker", "reduced_soft_hungarian"]
