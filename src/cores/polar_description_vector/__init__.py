36cd4ae38d01e3eae03dee30d5e7f3fccfee0015
Tran Quoc Hieu <ngoquochieu1211@gmail.com>
2025-10-22 09:36:54 +0700

    fix: optimize identification Our_implementation

--- src/cores/polar_description_vector/__init__.py
+++ src/cores/polar_description_vector/__init__.py
@@ -1,7 +1,7 @@
-from .process_polygon import compute_overlapping, construct_shape_vector, construct_sector, simplify_contour
+from .process_polygon import construct_shape_vector, construct_shape_vector_fast, construct_sector, compute_overlapping, simplify_contour
 from .shape_vector import ShapeVector
 
 __all__ = [
-    "compute_overlapping", "construct_shape_vector", "construct_sector", "simplify_contour",
-    "compute_movement", "check_valid_movement", "compute_distance", "ShapeVector"
+    "compute_overlapping", "construct_sector", "simplify_contour", 
+    "check_valid_movement", "ShapeVector", "construct_shape_vector_fast", "construct_shape_vector"
 ]
